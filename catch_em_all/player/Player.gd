a29e9fc4aabb3cb86aec164680adbd8e69b73ba0
Irwin1985 <rodriguez.irwin@gmail.com>
2020-01-26 23:25:48 +0100

    Parte 2 del Curso
    Parte 2 del curso

---
+++ catch_em_all/player/Player.gd
@@ -0,0 +1,45 @@
+extends Area2D
+
+var velocity = Vector2.ZERO
+var speed = 350
+
+
+func _ready():
+	OS.center_window()
+
+
+func _process(delta):
+	get_input()
+	position += velocity * delta
+	
+	position.x = clamp(position.x, 0, 480)
+	position.y = clamp(position.y, 0, 720)
+
+	process_animations()
+
+
+func get_input():
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = -1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y = 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+
+func process_animations():
+	if velocity.length() != 0:
+		$AnimatedSprite.play("run")
+		if velocity.x < 0:
+			$AnimatedSprite.flip_h = true
+		else:
+			$AnimatedSprite.flip_h = false
+	else:
+		$AnimatedSprite.play("idle")
+
+
