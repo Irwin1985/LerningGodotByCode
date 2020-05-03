c3442ec22964ca7b88e1047d006c6fb5b42916cb
Irwin Rodríguez <rodriguez.irwin@gmail.com>
2020-05-03 21:08:08 +0200

    player asset

--- Elevator Action/RestAPI.gd
+++ Elevator Action/RestAPI.gd
@@ -13,3 +13,5 @@ func _on_Button_pressed():
 func _on_HTTPRequest_request_completed(result, response_code, headers, body:PoolByteArray):
 	var json = JSON.parse(body.get_string_from_utf8())
 	$TextEdit.text = to_json(json.result)
+	var vec:Vector2
+	vec.length()
