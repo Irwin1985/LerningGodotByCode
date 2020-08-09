ee3ca645e71407e936aeb807faa84c61d28ad6a0
Irwin1985 <rodriguez.irwin@gmail.com>
2020-08-09 16:27:29 +0200

    botones touch

--- catch_em_all/player/Player.gd
+++ catch_em_all/player/Player.gd
@@ -8,6 +8,12 @@ signal hurt
 var velocity = Vector2.ZERO
 var speed = 350
 
+# touch section
+var touch_left = false
+var touch_right = false
+var touch_up = false
+var touch_down = false
+
 
 func _ready():
 	OS.center_window()
@@ -25,13 +31,14 @@ func _process(delta):
 
 func get_input():
 	velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_left"):
+	# si se pulsa la tecla izq. o se pulsa el boton touch izq.
+	if Input.is_action_pressed("ui_left") or touch_left:
 		velocity.x = -1
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right") or touch_right:
 		velocity.x = 1
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up") or touch_up:
 		velocity.y = -1
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down") or touch_down:
 		velocity.y = 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -66,12 +73,38 @@ func game_over():
 	$AnimatedSprite.animation = "hurt"
 
 
-
-
-
-
-
-
 func _on_Player_body_entered(body):
 	if body.is_in_group("enemy"):
 		emit_signal("hurt")
+
+
+func _on_LeftButton_pressed():
+	touch_left = true
+
+
+func _on_LeftButton_released():
+	touch_left = false
+
+
+func _on_RightButton_pressed():
+	touch_right = true
+
+
+func _on_RightButton_released():
+	touch_right = false
+
+
+func _on_UpButton_pressed():
+	touch_up = true
+
+
+func _on_UpButton_released():
+	touch_up = false
+
+
+func _on_BottomButton_pressed():
+	touch_down = true
+
+
+func _on_BottomButton_released():
+	touch_down = false
